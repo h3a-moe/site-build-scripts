7bf70895b4b10767d5bb2d3ee64dc288eba6b754
H3arn <root@h3a.moe>
2022-11-15 00:48:32 +0800

    test new friend link assembler

--- blog.sh
+++ blog.sh
@@ -5,5 +5,11 @@ npm install hexo-cli -g
 cd zh
 npm install --force
 
+if test -f "./source/_data/link.yml"; then
+    rm ./source/_data/link.yml
+fi
+
+cat ./source/_data/*.yml > ./source/_data/link.yml
+
 hexo clean
 hexo g
