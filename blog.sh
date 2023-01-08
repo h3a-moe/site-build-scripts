f1b78533d0f083768bb3a49dc2f0e99012e553b0
H3arn <root@h3a.moe>
2023-01-08 19:08:16 +0800

    add comments

--- blog.sh
+++ blog.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
+# Dependency: hexo-cli
 npm install hexo-cli -g
 
+# change workdir
 cd zh
+# install dependencies
 npm install --force
 
+# remove outdated link.yml
 if test -f "./source/_data/link.yml"; then
     rm ./source/_data/link.yml
 fi
-
+# generate friend link yaml file
 cat ./source/_data/*.yml > ./source/_data/link.yml
 
+# clean cache
 hexo clean
+# generate static files
 hexo g
