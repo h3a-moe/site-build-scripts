065d4da3f951e7912d60e38ca0c2f9e94e37c9e6
H3arn <root@h3a.moe>
2023-01-08 19:54:02 +0800

    add /history generator

--- blog.sh
+++ blog.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# get full commit history
+git fetch --unshallow
+
+# get PGP key
+wget https://github.com/h3arn.gpg 
+git config --global gpg.program gpg2
+gpg2 --import h3arn.gpg
+
 # Dependency: hexo-cli
 npm install hexo-cli -g
 
@@ -14,6 +22,21 @@ if test -f "./source/_data/link.yml"; then
 fi
 # generate friend link yaml file
 cat ./source/_data/*.yml > ./source/_data/link.yml
+
+# generate /history page
+mkdir -p source/history
+touch source/history/index.md 
+cat << EOT > source/history/index.md 
+---
+title: History
+---
+<div class="markdown-body">
+<h1>Commit History</h1>
+EOT
+echo '```' >> source/history/index.md 
+git log --show-signature >> source/history/index.md 
+echo '```' >> source/history/index.md 
+echo '</div>' >> source/history/index.md
 
 # clean cache
 hexo clean
