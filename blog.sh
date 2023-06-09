cae15cd5f3bbce528909397bac5903f89ab16e49
H3arn <root@h3a.moe>
2023-06-09 22:51:07 +0800

    replace `gpg2` with `gpg`

--- blog.sh
+++ blog.sh
@@ -3,15 +3,17 @@
 # get full commit history
 git fetch --unshallow
 
+gpg --version
+
 # get PGP key
 wget https://github.com/h3arn.gpg 
-git config --global gpg.program gpg2
-gpg2 --import h3arn.gpg
+#git config --global gpg.program gpg2
+gpg --import h3arn.gpg
 # set owner trust
-echo '364864A3A1AF4174D9BDE7C66076CBBA23E98A57:6:' | gpg2 --import-ownertrust
+echo '364864A3A1AF4174D9BDE7C66076CBBA23E98A57:6:' | gpg --import-ownertrust
 #gpg --list-keys --fingerprint |grep pub -A 1|egrep -Ev "pub|--"|tr -d ' '|sed 's/Keyfingerprint=//' \
 # | awk 'BEGIN { FS = "\n" } ; { print $1":6:" } ' | grep '364864A3A1AF4174D9BDE7C66076CBBA23E98A57:6:' | gpg --import-ownertrust
-gpg2 --list-keys
+gpg --list-keys
 # Dependency: hexo-cli
 npm install hexo-cli -g
 
